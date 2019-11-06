86d5a076eadb397f49cdd922c08a3abe7b458552
jcoronado4 <49005758+jcoronado4@users.noreply.github.com>
2019-11-05 19:09:12 -0600

    Add TXT file output when run

--- APIs/spotify_playlists_tracks.py
+++ APIs/spotify_playlists_tracks.py
@@ -1,9 +1,8 @@
-# This code uses the Spotify API and Spotipy library to create a CSV file for each of
-# my chosen playlists. Each CSV contains information for every track in the playlist
-# along with the date the track was added to the file. When rerun it appends any new 
-# tracks to the CSV. A new folder and files are created each year.
-
-# Future Updates: Automatically run this program at scheduled times.
+# This code starts by creating a CSV file for each of my chosen playlists.
+# Each CSV contains information for every track in the playlist along with 
+# the date the track was added to the file. When rerun it appends new 
+# tracks to the playlist. A new folder and files are created each year.
+# Outputs a TXT file containing times when the program was run.
 
 # Authentication Steps
 import spotipy.util as util
@@ -30,10 +29,10 @@ os.makedirs(sp_path, exist_ok = True)
 
 # Return Track Info as a String
 def get_track_info(tracks):
-	name = tracks['track']['name'].replace(",","|")
-	artist = tracks['track']['artists'][0]['name'].replace(",","|")
-	album = tracks['track']['album']['name'].replace(",","|")
-	return(name + "," + artist + "," + album)
+    name = tracks['track']['name'].replace(",","|")
+    artist = tracks['track']['artists'][0]['name'].replace(",","|")
+    album = tracks['track']['album']['name'].replace(",","|")
+    return(name + "," + artist + "," + album)
 
 # Creates a CSV File for Each Playlist
 def playlist_tracks_file(username, uri):
@@ -43,79 +42,77 @@ def playlist_tracks_file(username, uri):
     file_path = os.path.join(sp_path,filename)
     # If File Exists Append New Tracks
     if os.path.exists(file_path):
-    	f = open(file_path, 'r')
-    	csv_contents = f.read()
-    	f.close()
-    	f = open(file_path, 'a')
-    	tracks = results['tracks']['items']
-    	for i in range(len(tracks)):
-    		track_info = get_track_info(tracks[i])
-    		if not track_info in csv_contents:
-        		f.write(track_info + "," + today + "\n")
+        f = open(file_path, 'r')
+        csv_contents = f.read()
+        f.close()
+        f = open(file_path, 'a')
+        tracks = results['tracks']['items']
+        for i in range(len(tracks)):
+            track_info = get_track_info(tracks[i])
+            if not track_info in csv_contents:
+                f.write(track_info + "," + today + "\n")
     # Otherwise Create New File
     else:
-    	f = open(file_path, 'w')
-    	header = "Name" + "," + "Artist" + "," + "Album" + "," + "Date Added to File\n"
-    	f.write(header)
-    	tracks = results['tracks']['items']
-    	for i in range(len(tracks)):
-        	track_info = get_track_info(tracks[i])
-        	f.write(track_info + "," + today + "\n")
+        f = open(file_path, 'w')
+        header = "Name" + "," + "Artist" + "," + "Album" + "," + "Date Added to File\n"
+        f.write(header)
+        tracks = results['tracks']['items']
+        for i in range(len(tracks)):
+            track_info = get_track_info(tracks[i])
+            f.write(track_info + "," + today + "\n")
     f.close()
-    return
 
 # Creates a CSV File for my Liked Songs
 def liked_songs_tracks_file():
-	lim = 50
-	filename = "Liked Songs" + " - " + str(current_year) + ".csv"
-	file_path = os.path.join(sp_path,filename)
+    lim = 50
+    filename = "Liked Songs" + " - " + str(current_year) + ".csv"
+    file_path = os.path.join(sp_path,filename)
     # If File Exists Append New Tracks
-	if os.path.exists(file_path):
-		f = open(file_path, 'r')
-		csv_contents = f.read()
-		f.close()
-		f = open(file_path, 'a')
-		try:
-		    tracks = sp.current_user_saved_tracks(limit = lim)
-		    total_tracks = tracks['total']
-		    off_index = lim
-		    num_iter = total_tracks // lim
-		    for i in range(num_iter):
-		        for track in tracks['items']:
-		        	track_info= get_track_info(track)
-		        	if not track_info in csv_contents:
-		        		f.write(track_info + "," + today + "\n")
-		        tracks = sp.current_user_saved_tracks(limit = lim, offset = off_index)
-		        off_index = off_index + lim
-		    for track in tracks['items']:
-		    	track_info= get_track_info(track)
-		    	if not track_info in csv_contents:
-		    		f.write(track_info + "," + today + "\n")
-		except:
-		    pass
+    if os.path.exists(file_path):
+        f = open(file_path, 'r')
+        csv_contents = f.read()
+        f.close()
+        f = open(file_path, 'a')
+        try:
+            tracks = sp.current_user_saved_tracks(limit = lim)
+            total_tracks = tracks['total']
+            off_index = lim
+            num_iter = total_tracks // lim
+            for i in range(num_iter):
+                for track in tracks['items']:
+                    track_info= get_track_info(track)
+                    if not track_info in csv_contents:
+                        f.write(track_info + "," + today + "\n")
+                tracks = sp.current_user_saved_tracks(limit = lim, offset = off_index)
+                off_index = off_index + lim
+            for track in tracks['items']:
+                track_info= get_track_info(track)
+                if not track_info in csv_contents:
+                    f.write(track_info + "," + today + "\n")
+        except:
+            pass
     # Otherwise Create New File
-	else:
-		f = open(file_path, 'w')
-		header = "Name" + "," + "Artist" + "," + "Album" + "," + "Date Added to File\n"
-		f.write(header)
-		try:
-		    tracks = sp.current_user_saved_tracks(limit = lim)
-		    total_tracks = tracks['total']
-		    off_index = lim
-		    num_iter = total_tracks // lim
-		    for i in range(num_iter):
-		        for track in tracks['items']:
-		        	track_info= get_track_info(track)
-		        	f.write(track_info + "," + today + "\n")
-		        tracks = sp.current_user_saved_tracks(limit = lim, offset = off_index)
-		        off_index = off_index + lim
-		    for track in tracks['items']:
-		        track_info= get_track_info(track)
-		        f.write(track_info + "," + today + "\n")
-		except:
-		    pass
-	f.close()
-	return
+    else:
+        f = open(file_path, 'w')
+        header = "Name" + "," + "Artist" + "," + "Album" + "," + "Date Added to File\n"
+        f.write(header)
+        try:
+            tracks = sp.current_user_saved_tracks(limit = lim)
+            total_tracks = tracks['total']
+            off_index = lim
+            num_iter = total_tracks // lim
+            for i in range(num_iter):
+                for track in tracks['items']:
+                    track_info= get_track_info(track)
+                    f.write(track_info + "," + today + "\n")
+                tracks = sp.current_user_saved_tracks(limit = lim, offset = off_index)
+                off_index = off_index + lim
+            for track in tracks['items']:
+                track_info= get_track_info(track)
+                f.write(track_info + "," + today + "\n")
+        except:
+            pass
+    f.close()
 
 # Create "Today's Top Hits" CSV File 
 playlist_tracks_file("Spotify", "spotify:playlist:37i9dQZF1DXcBWIGoYBM5M")
@@ -134,3 +131,15 @@ playlist_tracks_file("Spotify", "spotify:playlist:37i9dQZF1DXcZDD7cfEKhW")
 
 # Create "Liked Songs" CSV File
 liked_songs_tracks_file()
+
+# Create Text File Showing When Code was Run
+filename = "spotify_api.txt"
+if os.path.exists(filename):
+    f = open(filename, 'a')
+    f.write(str(now) + "\n")
+else:
+    f = open(filename, 'w')
+    f.write("spotify_api.py was run at the following times: \n")
+    f.write("\n")
+    f.write(str(now) + "\n")
+f.close()
